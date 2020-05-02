adc967b74f3f713c6ed36cd0310ad133bac07a50
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-05-02 15:35:30 +0600

    Loading Views

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -1,12 +1,26 @@
 <?php
 
-class Pages
+use App\Libraries\Controller;
+
+class Pages extends Controller
 {
     public function __construct()
     {
-       echo "pages loaded";
+      
     }  
 
-    public function index(){}
+    public function index()
+    {
+        $data =  [
+            'title' => 'Welcome'
+        ];
+        
+       $this->view('pages/index', $data);
+    }
+
+    public function about()
+    {
+         $this->view('pages/about');
+    }
 
 }
